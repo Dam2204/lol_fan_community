f90cfb603f93dbc43f2b3572ac9a50dd02634515
Dam2204 <soyoung4576@gmail.com>
2025-05-11 17:39:34 +0900

    add: team data function

--- src/team/team.module.ts
+++ src/team/team.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
-import { TeamService } from './team.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
+import { Team } from './entities/team.entity';
 import { TeamController } from './team.controller';
+import { TeamService } from './team.service';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Team])],
   providers: [TeamService],
-  controllers: [TeamController]
+  controllers: [TeamController],
 })
 export class TeamModule {}
